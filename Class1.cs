910366e879674c483096112465d5e3c6012e81af
ignam19 <ignaciomersch602811@gmail.com>
2024-03-29 11:22:22 -0300

    añadido calse persona

--- Class1.cs
+++ Class1.cs
@@ -1,7 +1,24 @@
-﻿namespace ClassLibrary1
+﻿using System.Collections;
+using System.Collections.Specialized;
+
+namespace ClassLibrary1
 {
-    public class Class1
+    abstract class Persona
     {
+        public string nombre;
+        public string apellido;
+        public string dni;
+        public string direccion;
+        public string telefono;
+
+        Persona(string nombre, string apellido, string dni, string direccion, string telefono)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.dni = dni;
+            this.direccion = direccion;
+            this.telefono = telefono;
+        }
 
     }
 }
