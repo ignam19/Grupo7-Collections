91d4062a939222f94742d997c9bbfd5715e56ac7
ignam19 <ignaciomersch602811@gmail.com>
2024-03-29 11:48:21 -0300

    Revert "añadido calse persona"
    This reverts commit 910366e879674c483096112465d5e3c6012e81af.

--- Class1.cs
+++ Class1.cs
@@ -1,24 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Specialized;
-
-namespace ClassLibrary1
+﻿namespace ClassLibrary1
 {
-    abstract class Persona
+    public class Class1
     {
-        public string nombre;
-        public string apellido;
-        public string dni;
-        public string direccion;
-        public string telefono;
-
-        Persona(string nombre, string apellido, string dni, string direccion, string telefono)
-        {
-            this.nombre = nombre;
-            this.apellido = apellido;
-            this.dni = dni;
-            this.direccion = direccion;
-            this.telefono = telefono;
-        }
 
     }
 }
