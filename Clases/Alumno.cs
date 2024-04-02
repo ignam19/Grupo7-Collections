08dd949689f8c5014b4a0b531085c67c5d5954ff
Ignacio <ignaciomersch602811@gmail.com>
2024-04-02 14:55:41 -0300

    Añadir class Person y correcciones

---
+++ Clases/Alumno.cs
@@ -0,0 +1,37 @@
+﻿using System.Xml;
+
+namespace ClassLibrary1.Clases
+{
+    public class Alumno : Persona
+    {
+        public string Materia { get; set; }
+        public int Curso { get; set; }
+        public double Nota { get; set; }
+
+        public List<string> Materias { get; set; }
+
+        public Alumno(string materia, int curso, double nota, string firstName, string lastName, int age, List<string> materias)
+            : base(firstName, lastName, age)
+        {
+            Materia = materia;
+            Curso = curso;
+            Nota = nota;
+            Materias = materias;
+        }
+
+        public double calcularPromedio()
+        {
+            return Nota;
+        }
+
+        public void enviarTarea()
+        {
+
+        }
+
+        public void registarAsistencia()
+        {
+
+        }
+    }
+}
---
+++ Clases/Profesor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Clases
+{
+    public class Profesor : Persona
+    {
+        public int Sueldo {  get; set; }
+        public List<string> Materias { get; set; }
+        public int HorasCatedra { get; set; }
+
+        public Profesor(int sueldo, List<string> materias, int horasCatedra, string firstName, string lastName, int age)
+            : base(firstName, lastName, age)
+        {
+            Sueldo = sueldo;
+            Materias = materias;
+            HorasCatedra = horasCatedra;
+        }
+    }
+}
