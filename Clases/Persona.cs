9556ed7f8224902324f9cd5103f11f042bbb89f1
Ramiro Perichon <perichonramiro12@gmail.com>
2024-04-02 17:43:30 -0300

    add de class matermaticas and lengua

--- Clases/Persona.cs
+++ Clases/Persona.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary1.Clases
 {
-    public class Persona
+    abstract class Persona
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
